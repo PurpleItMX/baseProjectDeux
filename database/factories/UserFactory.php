892bab21021347da0ed5ad912219f8b92fe1bfb9
PurpleItMX <43585829+PurpleItMX@users.noreply.github.com>
2018-10-23 12:22:28 -0500

    menu creado

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,22 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'name' => 'matthew',//$faker->name,
+        'email' => 'mateo.vazquez@purpleit.com.mx', //$faker->unique()->safeEmail,
+        'password' => 'Matthew19', //'$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
 });
+
+/*$factory->define(App\Post::class, function ($faker) {
+    return [
+        'title' => $faker->title,
+        'content' => $faker->paragraph,
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'user_type' => function (array $post) {
+            return App\User::find($post['user_id'])->type;
+        }
+    ];
+});*/
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         //$this->call(UsersTableSeeder::class);
+    	$this->call([
+        	UserTableSeeder::class,
+        	RoleTableSeeder::class,
+        	MenuTableSeeder::class,
+        	MenuRoleTableSeeder::class,
+        	UserRoleTableSeeder::class,
+    	]);
     }
 }
