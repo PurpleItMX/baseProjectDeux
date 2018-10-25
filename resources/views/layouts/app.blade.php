4f0ab93cf89c20b1107cb7198248dfc7565bfe69
PurpleItMX <43585829+PurpleItMX@users.noreply.github.com>
2018-10-25 17:12:25 -0500

    parametros de correo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -10,17 +9,6 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>{{ config('app.name', 'Laravel') }} | Dashboard</title>
-    <!-------------------------- Styles ---------------------------------------------------->
-    <!-- Fonts -->
-    <link href="{{ URL::asset('css/Nunito-font.css') }}" rel="stylesheet" type="text/css">
-    <!--<link href="{{ URL::asset('css/Roboto-Varela-Round.css') }}" rel="stylesheet" type="text/css">-->
-    <link href="{{ URL::asset('css/font-awesome.min.css') }}" rel="stylesheet"  type="text/css">
-    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-    <!-- style app-->
-    <link href="{{ URL::asset('css/datatables.css') }}" rel="stylesheet">
-    <!--<link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">-->
-    <link href="{{ URL::asset('css/welcome.css') }}" rel="stylesheet">
-   
     <!------------------------------ Scripts ---------------------------------->
     <!-- jQuery -->    
     <script src="{{ URL::asset('js/jquery-3.3.1.min.js') }}"></script>       
@@ -30,45 +18,74 @@
     <script src="{{ URL::asset('js/holder.min.js') }}"></script>
     <script src="{{ URL::asset('js/datatables.min.js') }}"></script>
     <!-- js app -->
-    <!--<script src="{{ URL::asset('js/app.js') }}"></script>-->
-    
+    <script src="{{ URL::asset('js/app.js') }}"></script>
+
+    <!-------------------------- Styles ---------------------------------------------------->
+    <!-- Fonts -->
+    <link href="{{ URL::asset('css/Nunito-font.css') }}" rel="stylesheet" type="text/css">
+    <!--<link href="{{ URL::asset('css/Roboto-Varela-Round.css') }}" rel="stylesheet" type="text/css">-->
+    <link href="{{ URL::asset('css/font-awesome.min.css') }}" rel="stylesheet"  type="text/css">
+    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- style app-->
+    <link href="{{ URL::asset('css/datatables.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+    <!--<link href="{{ URL::asset('css/welcome.css') }}" rel="stylesheet">-->
 </head>
-<body class="hold-transition sidebar-mini">
-  <div class="wrapper">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand bg-white navbar-light border-bottom">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">Home</a>
-          </li>
-        </ul>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-         <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-          <!-- Messages Dropdown Menu -->
-          <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-              <i class="fa fa-comments-o"></i>
-              <span class="badge badge-danger navbar-badge">3</span>
-            </a>
-          </li>
-          <!-- Notifications Dropdown Menu -->
-          <li class="nav-item dropdown">
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
-                class="fa fa-th-large"></i></a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.navbar -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-      <main class="py-4">
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                @if (Route::has('register'))
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
+                            </li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!--<main class="py-4">
             @yield('content')
-        </main>
-
-  </div>
-  <!-- ./wrapper -->
+        </main>-->
+    </div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,11 +36,17 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Salir') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     @else
                         <a href="#myModalLogin" class="trigger-btn" data-toggle="modal">Acceder</a>
                         <a href="#myModalRegister" class="trigger-btn" data-toggle="modal">Registro</a>
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <!--<a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>-->
                     @endauth
                 </div>
             @endif
@@ -60,7 +66,7 @@
                         <div class="avatar">
                             <img src="{{ URL::asset('img/avatar.png') }}" alt="Avatar">
                         </div>              
-                        <h4 class="modal-title">Registre Acceso:</h4>   
+                        <h4 class="modal-title">{{ __('Registre Acceso') }}</h4>   
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
                     <div class="modal-body">
@@ -81,21 +87,29 @@
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>        
+                            </div> 
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">Accerder</button>
+                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">{{ __('Acceder') }}</button>
                             </div>
                         </form>
                     </div>
                     <div class="modal-footer">
-                        <a href="#">¿Olvido Contraseña?</a>
+                        <a href="#myModalRecover" data-toggle="modal">¿Olvido Contraseña?</a>
                     </div>
                 </div>
             </div>
         </div> 
        <!-- Fin Modal Login --> 
 
-       <!-- Modal Login  -->
+       <!-- Modal Register  -->
         <div id="myModalRegister" class="modal fade">
             <div class="modal-dialog modal-login">
                 <div class="modal-content">
@@ -103,42 +117,90 @@
                         <div class="avatar">
                             <img src="{{ URL::asset('img/avatar.png') }}" alt="Avatar">
                         </div>              
-                        <h4 class="modal-title">Registre Acceso:</h4>   
+                        <h4 class="modal-title">{{ __('Nuevo Acceso') }}</h4>   
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
                     <div class="modal-body">
-                        <form action="{{ route('login') }}" method="post">
+                        <form action="{{ route('register') }}" method="post">
                             @csrf
                             <div class="form-group">
-                                <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" placeholder="Correo" required="required" value="{{ old('email') }}">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"  placeholder="Nombre" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required placeholder="Correo">
+
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif     
-                            </div>
+                                @endif
+                            </div>   
                             <div class="form-group">
-                                <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Contraseña" required="required"> 
-                                 @if ($errors->has('password'))
+                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Contraseña">
+
+                                @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>        
+                            </div>   
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">Accerder</button>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeholder="Confime contraseña">
+                            </div>   
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">{{ __('Registrar') }}</button>
                             </div>
                         </form>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="#">¿Olvido Contraseña?</a>
                     </div>
                 </div>
             </div>
         </div> 
-       <!-- Fin Modal Login -->
+       <!-- Fin Modal Register -->
 
+       <!-- Modal Recover  -->
+        <div id="myModalRecover" class="modal fade">
+            <div class="modal-dialog modal-login">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <div class="avatar">
+                            <img src="{{ URL::asset('img/avatar.png') }}" alt="Avatar">
+                        </div>              
+                        <h4 class="modal-title">{{ __('Recuperar Acceso') }}</h4>   
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+                        <form action="{{ route('password.email') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required  placeholder="Correo">
 
-
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif 
+                            </div>     
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">{{ __('Enviar link reseo contraseña') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                    </div>
+                </div>
+            </div>
+        </div> 
+       <!-- Fin Modal Recover -->
     </body>
 </html>
