8b15365048ed79cda944966a54e1c28ff6a2be37
PurpleItMX <43585829+PurpleItMX@users.noreply.github.com>
2018-11-12 17:24:57 -0600

    change with foreingkey

--- database/migrations/2018_10_30_161356_create_supplies_table.php
+++ database/migrations/2018_10_30_161356_create_supplies_table.php
@@ -18,8 +18,8 @@ class CreateSuppliesTable extends Migration
             $table->string('clave')->unique();
             $table->string('description');
             $table->string('udm');
-            $table->Integer('id_supply_category');
-            $table->Integer('id_supply_type');
+            $table->Integer('id_supply_category')->unsigned();
+            $table->Integer('id_supply_type')->unsigned();
             $table->Integer('id_season');
             $table->string('tolerance');
             $table->string('stock_fixed');
@@ -31,10 +31,16 @@ class CreateSuppliesTable extends Migration
             $table->smallInteger('is_direct_purchase'); //si es compra directa
             $table->smallInteger('type'); //si es copeo producto unico o venta directa
             $table->smallInteger('estatus');
-            $table->Integer('id_provider_primary');
-            $table->Integer('id_provider_second');
-            $table->Integer('id_provider_third');
+            $table->Integer('id_provider_primary')->unsigned();
+            $table->Integer('id_provider_second')->unsigned();
+            $table->Integer('id_provider_third')->unsigned();
             $table->timestamps();
+
+            $table->foreign('id_supply_category')->references('id_supply_category')->on('supply_categories')->onDelete('cascade');
+            $table->foreign('id_supply_type')->references('id_supply_type')->on('supply_types')->onDelete('cascade');
+            $table->foreign('id_provider_primary')->references('id_provider')->on('providers')->onDelete('cascade');
+            $table->foreign('id_provider_second')->references('id_provider')->on('providers')->onDelete('cascade');
+            $table->foreign('id_provider_third')->references('id_provider')->on('providers')->onDelete('cascade');
         });
     }
 
