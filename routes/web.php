d61ea2699cbf67b675e6fc2397af481cc3b25fcc
PurpleItMX <43585829+PurpleItMX@users.noreply.github.com>
2018-12-14 09:11:19 -0600

    Access denied to mobile

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/denied', function () {
+    return view('no-access');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
